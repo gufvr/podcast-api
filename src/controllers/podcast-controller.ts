9b87281526f9900bd1de5807c2afbe2a4e9408ed
GuFvr <gustavo.vfavero@gmail.com>
2024-09-19 20:50:36 -0300

    feat: add query string handling for episode filtering by URL parameter

--- src/controllers/podcast-controller.ts
+++ src/controllers/podcast-controller.ts
@@ -17,7 +17,9 @@ export const getFilterEpisodes = async (
   req: IncomingMessage,
   res: ServerResponse
 ) => {
-  const content = await episodesFilterService('balela');
+  const queryString = req.url?.split('?p=')[1] || '';
+
+  const content = await episodesFilterService(queryString);
 
   res.writeHead(200, { 'Content-type': 'application/json' });
   res.end(JSON.stringify(content));
--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,13 @@ import {
 
 const server = http.createServer(
   async (req: http.IncomingMessage, res: http.ServerResponse) => {
-    if (req.method === 'GET' && req.url === '/api/list') {
+    const [baseUrl, queryString] = req.url?.split('?') ?? ['', ''];
+
+    if (req.method === 'GET' && baseUrl === '/api/list') {
       await getListEpisodes(req, res);
     }
 
-    if (req.method === 'GET' && req.url === '/api/episode') {
+    if (req.method === 'GET' && baseUrl === '/api/episode') {
       await getFilterEpisodes(req, res);
     }
   }
