d6ffb34de970e81421f5a97aee6febce84fc5328
GuFvr <gustavo.vfavero@gmail.com>
2024-09-19 20:27:08 -0300

    feat: add filtered GET endpoint to retrieve specific podcast episodes

--- src/controllers/podcast-controller.ts
+++ src/controllers/podcast-controller.ts
@@ -1,11 +1,23 @@
 import { IncomingMessage, request, ServerResponse } from 'http';
+
 import { serviceListEpisodes } from '../services/episodes-list-service';
+import { episodesFilterService } from '../services/episodes-filter-service';
 
 export const getListEpisodes = async (
   req: IncomingMessage,
   res: ServerResponse
 ) => {
   const content = await serviceListEpisodes();
+
+  res.writeHead(200, { 'Content-type': 'application/json' });
+  res.end(JSON.stringify(content));
+};
+
+export const getFilterEpisodes = async (
+  req: IncomingMessage,
+  res: ServerResponse
+) => {
+  const content = await episodesFilterService('balela');
 
   res.writeHead(200, { 'Content-type': 'application/json' });
   res.end(JSON.stringify(content));
